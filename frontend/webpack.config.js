733345f7193ac3be75e59ef383699630f5fd412e
Daniel Radetsky <dradetsky@gmail.com>
2017-06-17 17:19:24 -0700

    style

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,11 +3,13 @@ const webpack = require('webpack')
 const WebpackMd5Hash = require('webpack-md5-hash')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const config = {
   entry: {
     vendor: './src/vendor.js',
     main: './src/app.jsx',
+    style: './src/style.css',
   },
   output: {
     path: path.join(__dirname, 'public'),
@@ -25,7 +27,8 @@ const config = {
     }),
     new HtmlWebpackPlugin({
       template: './src/index.ejs',
-    })
+    }),
+    new ExtractTextPlugin('pack.[contenthash].css')
   ],
   module: {
     rules: [{
@@ -36,6 +39,29 @@ const config = {
         options: {
           presets: ['react', 'es2015']
         }
+      }
+    }, {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract({ use: 'css-loader'})
+    }, {
+      test: [
+          /\.svg/,
+          /\.eot/,
+          /\.ttf/,
+          /\.otf/,
+      ],
+      loader: 'file?name=fonts/[name].[ext]'
+    }, {
+      test: [
+          /\.woff/,
+          /\.woff2/
+      ],
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 50000,
+          mimetype: 'application/font-woff',
+        },
       }
     }]
   }
